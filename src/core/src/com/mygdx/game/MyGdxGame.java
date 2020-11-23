7d26f586fdc91749981ba4ffbe79c5294369bf17
Andy Killorin <speedy6451@gmail.com>
2020-11-23 12:29:29 -0600

    added orthographic camera and title text also made jar build, working on macos packaging next up is fonts and bundling

--- src/core/src/com/mygdx/game/MyGdxGame.java
+++ src/core/src/com/mygdx/game/MyGdxGame.java
@@ -3,19 +3,40 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture coinTexture1;
+	BitmapFont font;
 	Music rainMusic;
-	
+	OrthographicCamera camera;
+	public static int screenX = 800;
+	public static int screenY = 480; 
+
+	public enum GameState {
+		TITLE, 
+		MENU, 
+		CREDITS, 
+		LEVEL, 
+		LOSE
+	}; 
+	GameState gameState;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		// coinTexture1 = new Texture("");
+		font = new BitmapFont();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, screenX, screenY);
+		gameState = GameState.TITLE;
 
 		// load music
 		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("music/New Hope.ogg"));
@@ -26,9 +47,15 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		switch (gameState) {
+			case TITLE:
+				font.draw(batch, "Cool Game Title", screenX / 2, screenY / 2);
+				break;
+		}
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
