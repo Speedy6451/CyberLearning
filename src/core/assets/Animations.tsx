29d5ab196070a586c4316eeed1873c558ccde6a0
Andy Killorin <speedy6451@gmail.com>
2020-11-28 09:33:46 -0600

    added tilemaps, next is loading them.

---
+++ src/core/assets/Animations.tsx
@@ -0,0 +1,165 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Animations" tilewidth="16" tileheight="16" tilecount="35" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_1.png"/>
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/coin/coin_1.png"/>
+  <animation>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+   <frame tileid="6" duration="300"/>
+   <frame tileid="5" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/coin/coin_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/coin/coin_3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/candlestick_1_1.png"/>
+  <animation>
+   <frame tileid="8" duration="600"/>
+   <frame tileid="9" duration="600"/>
+   <frame tileid="10" duration="600"/>
+   <frame tileid="9" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/candlestick_1_2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/candlestick_1_3.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/candlestick_1_4.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/candlestick_2_1.png"/>
+  <animation>
+   <frame tileid="12" duration="600"/>
+   <frame tileid="13" duration="600"/>
+   <frame tileid="14" duration="600"/>
+   <frame tileid="13" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/candlestick_2_2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/candlestick_2_3.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/candlestick_2_4.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/side_torch_1.png"/>
+  <animation>
+   <frame tileid="15" duration="300"/>
+   <frame tileid="16" duration="300"/>
+   <frame tileid="17" duration="300"/>
+   <frame tileid="18" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/side_torch_2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/side_torch_3.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/side_torch_4.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/torch_1.png"/>
+  <animation>
+   <frame tileid="19" duration="300"/>
+   <frame tileid="20" duration="300"/>
+   <frame tileid="21" duration="300"/>
+   <frame tileid="22" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/torch_2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/torch_3.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/torch/torch_4.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/flasks/flasks_1_1.png"/>
+  <animation>
+   <frame tileid="23" duration="600"/>
+   <frame tileid="24" duration="600"/>
+   <frame tileid="25" duration="600"/>
+   <frame tileid="26" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/flasks/flasks_1_2.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/flasks/flasks_1_3.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/flasks/flasks_1_4.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/flasks/flasks_2_1.png"/>
+  <animation>
+   <frame tileid="27" duration="600"/>
+   <frame tileid="28" duration="600"/>
+   <frame tileid="29" duration="600"/>
+   <frame tileid="30" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="28">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/flasks/flasks_2_2.png"/>
+ </tile>
+ <tile id="29">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/flasks/flasks_2_3.png"/>
+ </tile>
+ <tile id="30">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/flasks/flasks_2_4.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/keys/keys_1_1.png"/>
+  <animation>
+   <frame tileid="31" duration="600"/>
+   <frame tileid="32" duration="600"/>
+   <frame tileid="34" duration="600"/>
+   <frame tileid="33" duration="600"/>
+   <frame tileid="34" duration="600"/>
+   <frame tileid="32" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/keys/keys_1_2.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/keys/keys_1_3.png"/>
+ </tile>
+ <tile id="34">
+  <image width="16" height="16" source="2D Pixel Dungeon Asset Pack/items and trap_animation/keys/keys_1_4.png"/>
+ </tile>
+</tileset>
---
+++ src/core/assets/Dungeon_Tileset.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Dungeon_Tileset" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="2D Pixel Dungeon Asset Pack/character and tileset/Dungeon_Tileset.png" width="160" height="160"/>
+ <terraintypes>
+  <terrain name="floor" tile="0"/>
+ </terraintypes>
+ <tile id="6" terrain="0,0,0,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,0,0,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,0,0,0"/>
+ <tile id="18" terrain="0,0,0,0"/>
+ <tile id="19" terrain="0,0,0,0"/>
+ <tile id="22" terrain="0,0,0,0"/>
+ <tile id="23" terrain="0,0,0,0"/>
+ <tile id="26" terrain="0,0,0,0"/>
+ <tile id="27" terrain="0,0,0,0"/>
+ <tile id="28" terrain="0,0,0,0"/>
+ <tile id="29" terrain="0,0,0,0"/>
+ <tile id="70" terrain="0,0,0,0"/>
+ <tile id="71" terrain="0,0,0,0"/>
+ <tile id="72" terrain="0,0,0,0"/>
+ <tile id="73" terrain="0,0,0,0"/>
+</tileset>
